5942b8f7394bb7d032628ac8bcb1578f7c1b264c
Matthew J. Morrison <matt@ascentus.com>
2018-01-16 02:46:35 -0500

    Logo? For what lol

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Konami from './konami';
 import KonamiStarWars from './konami-star-wars';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
